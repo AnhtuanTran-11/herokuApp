008ee3d6fc3d886946bdeae1fd95841d532d098d
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-04-08 14:36:57 -0500

    ready for deployment

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const server = express()
+
+server.use(express.json())
+
+if (process.env.NODE_ENV === "development") { // on Heroku machine, an env variable is called "NODE_ENV" with the value of "production"
+    const cors = require('cors')
+    server.use(cors())
+}
+
+server.use('*', (req, res) => {
+    res.send('<h1>Success</h1>')
+})
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
