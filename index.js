99dcc0de81810b63b4b335d5967bb20d65ee9c21
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-04-08 16:09:50 -0500

    wip

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-require('dotenv').config()
-const express = require('express')
-const server = express()
+require("dotenv").config();
+const path = require("path");
+const express = require("express");
+const server = express();
 
-server.use(express.json())
+server.use(express.json());
+server.use(express.static(path.join(__dirname, "client/build")));
 
-if (process.env.NODE_ENV === "development") { // on Heroku machine, an env variable is called "NODE_ENV" with the value of "production"
-    const cors = require('cors')
-    server.use(cors())
+if (process.env.NODE_ENV === "development") {
+  // on Heroku machine, an env variable is called "NODE_ENV" with the value of "production"
+  const cors = require("cors");
+  server.use(cors());
 }
 
-server.use('*', (req, res) => {
-    res.send('<h1>Success</h1>')
-})
+server.get("/api/hello", (req, res) => {
+  res.json({
+    message: "hello",
+  });
+});
 
-const PORT = process.env.PORT || 4000
+// Catch-all that just sends back index.html
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
+
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`)
-})
+  console.log(`Listening on ${PORT}`);
+});
